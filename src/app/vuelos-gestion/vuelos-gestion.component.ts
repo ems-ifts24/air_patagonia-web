900306cab92342ed727eccca86ef5dd17c2a933d
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-05-22 20:49:29 -0300

    Primeros componentes e imagenes

---
+++ src/app/vuelos-gestion/vuelos-gestion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vuelos-gestion',
+  imports: [],
+  templateUrl: './vuelos-gestion.component.html',
+  styleUrl: './vuelos-gestion.component.css'
+})
+export class VuelosGestionComponent {
+
+}
---
+++ src/app/vuelos-listado/vuelos-listado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vuelos-listado',
+  imports: [],
+  templateUrl: './vuelos-listado.component.html',
+  styleUrl: './vuelos-listado.component.css'
+})
+export class VuelosListadoComponent {
+
+}
