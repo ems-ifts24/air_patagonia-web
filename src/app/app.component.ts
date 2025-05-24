394f69ffabe7431b6536d317f8d7c5ccef541863
MatiasMcCarthy <matias.mccarthy@gmail.com>
2025-05-24 19:01:43 -0300

    Agrego los cambios en el html y css para la vista de dashboard incorporando media queries para hacer responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+<<<<<<< Updated upstream
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+=======
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [LoginComponent,DashboardComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
