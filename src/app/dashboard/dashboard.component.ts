3fabebcca0033977d3ecc679b30591da70be49d7
roodiaz <roabigail.diaz@gmail.com>
2025-05-28 20:31:37 -0300

    se agrega sidebar y header a componente dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule, HeaderComponent, SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
