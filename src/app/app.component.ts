c4329ed611dbbe3204072a100399affa84ad0145
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-05-31 00:01:58 -0300

    Limpio app.component en favor de layout.component para renderizar las distintas secciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { DashboardComponent } from "./dashboard/dashboard.component";
-
-
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, LoginComponent, HeaderComponent, DashboardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'air_patagonia-web';
 }
