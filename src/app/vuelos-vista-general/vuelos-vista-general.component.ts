3252b0b13c51a910e745c43185a1b48b87966cc7
Ezequiel Selis <ezequiel.selis@gmail.com>
2025-06-02 08:54:42 -0300

    En vista general vuelos, se crea un array con datos random para vuelos y se modifican estilos de los botones editar y eliminar.

--- src/app/vuelos-vista-general/vuelos-vista-general.component.ts
+++ src/app/vuelos-vista-general/vuelos-vista-general.component.ts
@@ -8,6 +8,109 @@ import { Router } from '@angular/router';
   styleUrl: './vuelos-vista-general.component.css'
 })
 export class VuelosVistaGeneralComponent {
+  vuelos: any[] = [
+    {
+      id: '1',
+      vuelo: 'AP1234',
+      modelo: '747',
+      origen: 'Buenos Aires',
+      destino: 'Bariloche',
+      partida: '25/05/2025',
+      arribo: '25/05/2025',
+      estado: 'Finalizado'
+    },
+    {
+      id: '2',
+      vuelo: 'AP1235',
+      modelo: 'A320',
+      origen: 'Córdoba',
+      destino: 'Ushuaia',
+      partida: '26/05/2025',
+      arribo: '26/05/2025',
+      estado: 'En vuelo'
+    },
+    {
+      id: '3',
+      vuelo: 'AP1236',
+      modelo: '737',
+      origen: 'Rosario',
+      destino: 'El Calafate',
+      partida: '27/05/2025',
+      arribo: '27/05/2025',
+      estado: 'Programado'
+    },
+    {
+      id: '4',
+      vuelo: 'AP1237',
+      modelo: '787',
+      origen: 'Mendoza',
+      destino: 'Bariloche',
+      partida: '28/05/2025',
+      arribo: '28/05/2025',
+      estado: 'Programado'
+    },
+    {
+      id: '5',
+      vuelo: 'AP1238',
+      modelo: 'A330',
+      origen: 'Buenos Aires',
+      destino: 'Ushuaia',
+      partida: '29/05/2025',
+      arribo: '29/05/2025',
+      estado: 'En vuelo'
+    },
+    {
+      id: '6',
+      vuelo: 'AP1239',
+      modelo: '777',
+      origen: 'Córdoba',
+      destino: 'El Calafate',
+      partida: '30/05/2025',
+      arribo: '30/05/2025',
+      estado: 'Programado'
+    },
+    {
+      id: '7',
+      vuelo: 'AP1240',
+      modelo: '747',
+      origen: 'Rosario',
+      destino: 'Bariloche',
+      partida: '31/05/2025',
+      arribo: '31/05/2025',
+      estado: 'Programado'
+    },
+    {
+      id: '8',
+      vuelo: 'AP1241',
+      modelo: 'A320',
+      origen: 'Mendoza',
+      destino: 'Ushuaia',
+      partida: '01/06/2025',
+      arribo: '01/06/2025',
+      estado: 'En vuelo'
+    },
+    {
+      id: '9',
+      vuelo: 'AP1242',
+      modelo: '737',
+      origen: 'Buenos Aires',
+      destino: 'El Calafate',
+      partida: '02/06/2025',
+      arribo: '02/06/2025',
+      estado: 'Programado'
+    },
+    {
+      id: '10',
+      vuelo: 'AP1243',
+      modelo: '787',
+      origen: 'Córdoba',
+      destino: 'Bariloche',
+      partida: '03/06/2025',
+      arribo: '03/06/2025',
+      estado: 'Programado'
+    }
+  ];
+
   constructor(private router: Router) { }
 
   crearVuelo(){
