3af80dc1e1d245cbb8f891624dc2c5f0a890db92
roodiaz <roabigail.diaz@gmail.com>
2025-06-03 19:09:45 -0300

    se modifica listado de empleados en gestion de vuelos

--- src/app/vuelos-gestion/vuelos-gestion.component.ts
+++ src/app/vuelos-gestion/vuelos-gestion.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { VueloServiceService, Empleado } from '../core/services/vuelo-service.service';
 
 @Component({
   selector: 'app-vuelos-gestion',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './vuelos-gestion.component.html',
   styleUrl: './vuelos-gestion.component.css'
 })
-export class VuelosGestionComponent {
-  constructor(private router: Router) { }
+export class VuelosGestionComponent implements OnInit { // Implementa OnInit
 
-titleCreacion = 'CREACION';
-titleModificacion='MODIFICACION';
+  titleCreacion = 'CREACION';
+  titleModificacion = 'MODIFICACION';
 
-cancelarModificacion(){
+  listaEmpleadosParaAsignar: Empleado[] = [];
+  rolesDisponibles: string[] = [
+    'Piloto',
+    'Copiloto',
+    'Tripulante',
+    'Jefe de Cabina',
+  ];
+
+  // inyecto el servicio
+  constructor(private router: Router, private vueloService: VueloServiceService) { }
+
+  ngOnInit(): void {
+    const empleadosDelServicio = this.vueloService.getEmpleados();
+    this.listaEmpleadosParaAsignar = empleadosDelServicio.map(emp => ({
+      ...emp, // Copia todas las propiedades del empleado original
+    }));
+  }
+
+  cancelarModificacion() {
     this.router.navigate(['app/vuelos']);
-}
+  }
 }
